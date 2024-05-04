d13bc49eb312987ac3310f621cf9dba003fa4f9a
Carlos Rivera <riveramoralescarlosandres25@gmail.com>
2024-05-04 13:20:48 -0500

    Se termina punto 3

---
+++ creacion-graficos/semana_9/punto3/main.py
@@ -0,0 +1,33 @@
+import pandas as pd
+
+df = pd.read_csv('./yarleanspuntocoom.csv')
+
+#1
+conteo_cargos = df[df['Cargo'].isin(['Chef', 'Mesero'])].groupby('Sede')['Cargo'].value_counts().unstack()
+
+sede_requerida = conteo_cargos[(conteo_cargos['Chef'] == 3) & (conteo_cargos['Mesero'] == 5)].index.tolist()
+
+print('La sede que tiene 3 chefs y 5 meseros es:')
+print(sede_requerida)
+
+#2
+salarios_por_sede = df.groupby('Sede')['Salario'].sum().reset_index(name='Total de salarios')
+
+print("\nTotal de salarios por sede:")
+print(salarios_por_sede)
+
+#3
+promedio_salarios = df['Salario'].mean()
+print("\nEl promedio de salarios es:", promedio_salarios)
+
+#4
+suma_total_salarios = df['Salario'].sum()
+print("La suma total de salarios es:", suma_total_salarios)
+
+
+
+#Punto de 4
+""" 
+
+
+"""
